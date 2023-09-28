a00f9d1f7f86315bc900e3fbc54e6761652d3250
unknown <farizfirjatullah@gmail.com>
2023-09-28 23:12:01 +0700

    tumbal proyek -mas gilang

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path'); //tidak perlu npm install
 
 // init express server and router
 const app = express();
-// const mainRouter = require('./app/routes');
+const mainRouter = require('./app/routes');
 
 app.use(express.json()); // supaya express bisa response json
 app.use(express.urlencoded({ extended: false })); // supaya express bisa menerima body
@@ -12,8 +12,9 @@ app.use(express.urlencoded({ extended: false })); // supaya express bisa menerim
 //     console.log("ini middleware")
 // })
 
-// // http router
-// app.use("/", mainRouter);
+// http router
+app.use("/", mainRouter);
+
 const port = 3000
 app.listen(port, function(){
     console.log("server start on", port)
