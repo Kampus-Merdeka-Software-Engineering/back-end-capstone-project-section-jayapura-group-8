c5a75c3ceb814d1118475ee300d1f735467b3693
Gilang Alfitra <gilangalfitra5@gmail.com>
2023-10-07 15:29:47 +0700

    update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false })); // supaya express bisa menerim
 // http router
 app.use("/", mainRouter);
 
-const port = 3000
+const port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log("server start on", port)
     connection.authenticate()
